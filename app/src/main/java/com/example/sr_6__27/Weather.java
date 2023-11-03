ceeba4198ef166dd144243086b1bb6537a782e62
Diana <7819shalp@gmail.com>
2023-11-03 22:14:24 +0700

    git commit push24567

---
+++ app/src/main/java/com/example/sr_6__27/Weather.java
@@ -0,0 +1,31 @@
+package com.example.sr_6__27;
+
+public class Weather {
+    String date;
+    String temp;
+    String we;
+    public Weather(String date, String temp, String we)
+    {
+        this.date= date;
+        this.temp= temp;
+        this.we= we;
+    }
+    public String GetDate()
+    {
+        return this.date;
+    }
+    public void SetDate(String date)
+    {
+        this.date= date;
+    }
+    public String GetUrl()
+    {
+        return this.we;
+    }
+    public String GetTemp()
+    {
+        return this.temp;
+    }
+
+
+}
